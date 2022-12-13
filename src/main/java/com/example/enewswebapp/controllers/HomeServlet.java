12c1762501ba5a45bc7e69cd23f06b52a2273f7b
ACER <ACER@JASON>
2022-12-13 11:47:43 +0700

    Home Servlet

---
+++ src/main/java/com/example/enewswebapp/controllers/HomeServlet.java
@@ -0,0 +1,32 @@
+package com.example.enewswebapp.controllers;
+
+import com.example.enewswebapp.utils.ServletUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/HomeServlet")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String path = request.getPathInfo();
+    if (path == null || path.equals("/")){
+        path = "/Index";
+    }
+    switch (path){
+        case "/Index":
+            ServletUtils.forward("/Views/vwHome/Index.jsp", request, response);
+            break;
+        default:
+            ServletUtils.forward("/Views/404.jsp", request, response);
+            break;
+    }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
