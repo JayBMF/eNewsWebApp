6d5f5326d3072ac7b80733ad35bc8ca7b1953fe0
ACER <ACER@JASON>
2022-12-13 12:10:17 +0700

    Home View

--- src/main/java/com/example/enewswebapp/controllers/HomeServlet.java
+++ src/main/java/com/example/enewswebapp/controllers/HomeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "HomeServlet", value = "/HomeServlet")
+@WebServlet(name = "HomeServlet", value = "/Home/*")
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
